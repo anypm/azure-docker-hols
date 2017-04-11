f917c029bdd52d476d1330f3d61fde785ddf1807
gjy <1019032915@qq.com>
2017-04-11 07:40:34 +0000

    Updated install-sphinx.sh

--- install-sphinx.sh
+++ install-sphinx.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get install -y python-pip python-dev build-essential 
 sudo pip install --upgrade pip 
 sudo pip install --upgrade virtualenv 
-sudo pip install Sphinx
-sudo pip install sphinx_rtd_theme
+pip install Sphinx
+pip install sphinx_rtd_theme
